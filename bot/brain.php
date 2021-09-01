653a6bc3ca260a6a5649efca723e46c63b1ccc59
chipslays <hello@chipslays.ru>
2021-09-01 12:15:40 +0400

    fix typo

--- bot/brain.php
+++ bot/brain.php
@@ -2,7 +2,7 @@
 
 $bot->command('start {:source?}', 'BotController@start');
 $bot->command('help', 'BotController@help');
-$bot->command('settings', 'BotController@help');
+$bot->command('settings', 'BotController@settings');
 
 $bot->command('litegram', function () {
     $this->say('Litegram v3.x.x');
