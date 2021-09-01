361538afa808707aa0af30037117229639aaef25
chipslays <hello@chipslays.ru>
2021-09-01 12:10:07 +0400

    update

--- public/index.php
+++ public/index.php
@@ -1,34 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Litegram</title>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+    </head>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Litegram</title>
-
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-
-</head>
-
-<body class="bg-gray-900 text-gray-200">
-    <div class="max-w-xl mx-auto w-full mt-8">
-        <div class="rounded-lg border-4 border-gray-700 border-dashed p-4 text-center">
-            <div class="text-4xl font-black uppercase">
-                Litegram
-            </div>
-            <div class="text-sm font-medium text-green-500 mt-4 animate-pulse">
-                now is online
-            </div>
-
-            <div class="mt-4 text-sm font-medium uppercase border-t-2 border-gray-700 pt-4">
-                <a href="https://github.com/chipslays/litegram" target="_blank" class="text-gray-400 hover:text-gray-500 transition duration-200">Github</a>
-                <span class="text-gray-500">•</span>
-                <a href="https://github.com/chipslays/litegram/tree/v3.x.x/docs/ru" target="_blank" class="text-gray-400 hover:text-gray-500 transition duration-200">Docs v3.x</a>
+    <body class="bg-gray-900 text-gray-200">
+        <div class="max-w-xl mx-auto w-full mt-8">
+            <div class="rounded-lg border-4 border-gray-700 border-dashed p-4 text-center">
+                <div class="text-4xl font-black uppercase">Litegram</div>
+                <div class="text-sm font-medium text-green-500 mt-4 animate-pulse">now is online</div>
+                <div class="mt-4 text-sm font-medium uppercase border-t-2 border-gray-700 pt-4">
+                    <a href="https://github.com/chipslays/litegram" target="_blank" class="text-gray-400 hover:text-gray-500 transition duration-200">Github</a> <span class="text-gray-500">•</span>
+                    <a href="https://github.com/chipslays/litegram/tree/v3.x.x/docs/ru" target="_blank" class="text-gray-400 hover:text-gray-500 transition duration-200">Docs v3.x</a>
+                </div>
             </div>
         </div>
-    </div>
-</body>
-
+    </body>
 </html>
