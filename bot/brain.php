6731350923a488bb4806eb9f87375ed30d53e61e
chipslays <hello@chipslays.ru>
2021-09-02 19:09:20 +0400

    fix

--- bot/brain.php
+++ bot/brain.php
@@ -4,6 +4,10 @@ $bot->command('start {:source?}', 'BotController@start');
 $bot->command('help', 'BotController@help');
 $bot->command('settings', 'BotController@settings');
 
+$bot->default('message', function () {
+    reply('Oh, I dont know what to say to that.');
+});
+
 $bot->command('litegram', function () {
-    $this->say('Litegram v3.x.x');
+    say('Litegram v3.x.x');
 });
